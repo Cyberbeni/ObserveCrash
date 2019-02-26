67f1b1a5a61954675e039924f3224d555f5391da
Benedek Kozma <cyberbeni@gmail.com>
2019-02-26 22:04:38 +0100

    Initial Commit

---
+++ Observe Crash/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Observe Crash
+//
+//  Created by Benedek Kozma on 2019. 02. 26..
+//  Copyright © 2019. Benedek Kozma. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
